229c5ecd230024deeb5657da41e64ad9c23bf11c
eng-software <henrique.coser@gmail.com>
2021-03-25 15:00:35 -0300

    - Incluido texto de informações de distribuição do software

--- Program.cs
+++ Program.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/*
+   This example code is in the Public Domain
+
+   This software is distributed on an "AS IS" BASIS, 
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+   either express or implied.
+
+   Este código de exemplo é de uso publico,
+
+   Este software é distribuido na condição "COMO ESTÁ",
+   e NÃO SÃO APLICÁVEIS QUAISQUER GARANTIAS, implicitas 
+   ou explicitas
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
